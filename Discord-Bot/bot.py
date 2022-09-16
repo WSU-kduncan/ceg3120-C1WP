55b0cdecb1e8e94a30c0684594cc00ae82a46c6f
Jimmy <jmcaupp@gmail.com>
2022-09-16 11:32:08 -0400

    final fix

--- Discord-Bot/bot.py
+++ Discord-Bot/bot.py
@@ -40,41 +40,21 @@ async def on_message(message):
     ]
 
     psych_quotes = [
-        'Gus, dont be the tender sweetness of a seasick crocodile.',
-        'Gus, dont be the very model of a modern major general.',
-        'Gus, dont be your jury summons that I accidentally threw away last month along with something called a W-2.',
-        'Gus, dont be Nic Cages accent from Con Air.',
-        'Gus, dont be an old sponge with hair hanging off of it.',
-        'Gus, dont be exactly half of an eleven-pound black forest ham.',
-        'Gus, dont be a rabid porcupine.',
+        'the tender sweetness of a seasick crocodile.',
+        'the very model of a modern major general.',
+        'your jury summons that I accidentally threw away last month along with something called a W-2.',
+        'Nic Cage\'s accent from Con Air.',
+        'an old sponge with hair hanging off of it.',
+        'exactly half of an eleven-pound black forest ham.',
+        'a rabid porcupine.',
     ]
     if message.content == 'towel!':
     #if message.content.startswith('$towel'):
         response = random.choice(hitchhiker_quotes)
         await message.channel.send(response)
     
-    if message.content == 'gdb!':
+    if message.content == 'Gus, don\'t be':
     #if message.content.startswith('$gdb!'):
-        response = random.choice(psych_quotes)
-        await message.channel.send(response)
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    psych_quotes = [
-        'a gooey chocolate chip cookie.',
-        'an incorrigible Eskimo pie with a caramel ribbon."',
-        'a rabid porcupine.',
-        'exactly half of an eleven-pound black forest ham.',
-        'the American adaptation of the British Gus.',
-        'Nic Cage\'s accent from Con Air.',
-        'your jury summons that I accidentally threw away last month along with something called a W-2.',
-        'the tender sweetness of a seasick crocodile.',
-        'the very model of a modern major general.',
-    ]
-    if message.content == 'Gus, dont be':
         response = random.choice(psych_quotes)
         await message.channel.send(response)
 
