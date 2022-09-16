b6d001bfa222ed26710e07192603c0974f69f127
Jimmy <jmcaupp@gmail.com>
2022-09-16 10:34:07 -0400

    added quotes

--- Discord-Bot/bot.py
+++ Discord-Bot/bot.py
@@ -43,4 +43,24 @@ async def on_message(message):
         response = random.choice(hitchhiker_quotes)
         await message.channel.send(response)
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    psych_quotes = [
+        'a gooey chocolate chip cookie.',
+        'an incorrigible Eskimo pie with a caramel ribbon."',
+        'a rabid porcupine.',
+        'exactly half of an eleven-pound black forest ham.',
+        'the American adaptation of the British Gus.',
+        'Nic Cage\'s accent from Con Air.',
+        'your jury summons that I accidentally threw away last month along with something called a W-2.',
+        'the tender sweetness of a seasick crocodile.',
+        'the very model of a modern major general.',
+    ]
+    if message.content == 'Gus, dont be':
+        response = random.choice(psych_quotes)
+        await message.channel.send(response)
+
 client.run(TOKEN)
